d8673838eac0c8d9fba1c14160f850bd1811c0b3
Peter Sedgewick <peter.sedgewick@gmail.com>
2015-12-10 16:00:13 +0000

    Changing styling on buttons / text.

--- app/src/main/java/com/count/countr/gui/DecrementButton.java
+++ app/src/main/java/com/count/countr/gui/DecrementButton.java
@@ -2,6 +2,7 @@ package com.count.countr.gui;
 
 import android.content.Context;
 import android.graphics.PorterDuff;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -17,20 +18,30 @@ public class DecrementButton extends CountButton
         super(context);
 
         Button b = this.getButtonInstance();
-        setText("-");
+//        setText("-");
         b.getBackground().setColorFilter(context.getResources().getColor(R.color.button_red), PorterDuff.Mode.MULTIPLY);
         GridLayout.LayoutParams lp = getLayoutParams();
-        lp.setMargins(0, 150, 0, 10);
+        lp.setMargins(0, 200, 0, 10);
+        lp.height = 125;
         b.setLayoutParams(lp);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CountRow row = getCountRowFromButton(view);
-
                 row.decrement();
-
                 redraw(view);
+            }
+        });
+
+        b.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev) {
+                if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+                    view.performClick();
+                }
+
+                return true;
             }
         });
     }
--- app/src/main/java/com/count/countr/gui/IncrementButton.java
+++ app/src/main/java/com/count/countr/gui/IncrementButton.java
@@ -2,12 +2,15 @@ package com.count.countr.gui;
 
 import android.content.Context;
 import android.graphics.PorterDuff;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 
 import com.count.countr.CountRow;
 import com.count.countr.R;
+
+import java.util.concurrent.Callable;
 
 public class IncrementButton extends CountButton
 {
@@ -16,22 +19,32 @@ public class IncrementButton extends CountButton
         super(context);
 
         Button b = this.getButtonInstance();
-        setText("+");
+//        setText("+");
         b.getBackground().setColorFilter(context.getResources().getColor(R.color.button_green), PorterDuff.Mode.MULTIPLY);
 
         GridLayout.LayoutParams lp = getLayoutParams();
         lp.setMargins(0, 20, 0, 0);
         lp.rowSpec = GridLayout.spec(0);
+        lp.height = 200;
         b.setLayoutParams(lp);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CountRow row = getCountRowFromButton(view);
-
                 row.increment();
-
                 redraw(view);
+            }
+        });
+
+        b.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev) {
+                if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+                    view.performClick();
+                }
+
+                return true;
             }
         });
     }
